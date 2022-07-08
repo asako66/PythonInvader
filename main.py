49eb1e8cc35bb37f6f98516d7c7e85e96edbdba8
asako66 <arketing6429@gmail.com>
2022-07-08 14:55:58 +0900

    Create Player

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+from tokenize import group
 import pygame
 from pygame.locals import *
 import sys
 
 SCREEN = Rect((0,0,640,480))
+
+class Player(pygame.sprite.Sprite):
+  SPEED = 5
+
+  def __init__(self):
+    pygame.sprite.Sprite.__init__(self, self.containers)
+    self.image = pygame.image.load("picture/player01.png").convert_alpha()
+    self.image = pygame.transform.scale(self.image, (56,56))
+    self.rect = self.image.get_rect()
+    self.rect.bottom = SCREEN.bottom
+    self.rect.centerx = SCREEN.centerx
+
+  def update(self):
+    pressed_keys = pygame.key.get_pressed()
+    if pressed_keys[K_LEFT]:
+      self.rect.move_ip(-Player.SPEED, 0)
+    elif pressed_keys[K_RIGHT]:
+      self.rect.move_ip(Player.SPEED, 0)
+    self.rect.clamp_ip(SCREEN)
 
 def main():
   # 初期設定
@@ -14,14 +34,20 @@ def main():
   # 登場する人もの背景の作成
 
   # Sprite登録
+  group = pygame.sprite.RenderUpdates()
+  Player.containers = group
+
+  Player()
 
   while True:
     # 画面（screen）をクリア
     screen.fill((0,0,0))
 
     # ゲームに登場する人もの背景の位置Update
+    group.update()
 
     # 画面（screen）上に登場する人もの背景を描画
+    group.draw(screen)
 
     # 画面（screen）の実表示
     pygame.display.update()
