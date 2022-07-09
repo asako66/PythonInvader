f469f2e88bb7f9d48fc368267ef56571ae10dfc5
asako66 <arketing6429@gmail.com>
2022-07-09 15:09:21 +0900

    Create Beam

--- main.py
+++ main.py
@@ -24,6 +24,27 @@ class Player(pygame.sprite.Sprite):
       self.rect.move_ip(Player.SPEED, 0)
     self.rect.clamp_ip(SCREEN)
 
+class Beam(pygame.sprite.Sprite):
+  """ビーム"""
+  SPEED = 5
+  counter = 0
+
+  def __init__(self,player):
+    pygame.sprite.Sprite.__init__(self, self.containers)
+    self.image = pygame.image.load("picture/beam_pink2.png").convert_alpha()
+    self.image = pygame.transform.scale(self.image, (56,56))
+    self.image = pygame.transform.rotate(self.image, 90)
+    self.rect = self.image.get_rect()
+    self.player = player
+    self.rect.center = self.player.rect.center
+
+  def update(self):
+    self.rect.move_ip(0, -Beam.SPEED)
+    if self.rect.top < 0:
+      Beam.counter -= 1
+      self.kill()
+
+
 def main():
   # 初期設定
   pygame.init()
@@ -36,8 +57,9 @@ def main():
   # Sprite登録
   group = pygame.sprite.RenderUpdates()
   Player.containers = group
+  Beam.containers = group
 
-  Player()
+  player = Player()
 
   while True:
     # 画面（screen）をクリア
@@ -57,9 +79,15 @@ def main():
       if event.type == QUIT:
         pygame.quit()
         sys.exit()
-      if event.type == KEYDOWN and event.key == K_ESCAPE:
-        pygame.quit()
-        sys.exit()
+      if event.type == KEYDOWN:
+        if event.key == K_ESCAPE:
+          pygame.quit()
+          sys.exit()
+        if event.key == K_SPACE:
+          """ビームの発射"""
+          if Beam.counter < 3:
+            Beam.counter += 1
+            Beam(player)
 
     # 描画スピードの調整（FPS）
     clock.tick(60)
